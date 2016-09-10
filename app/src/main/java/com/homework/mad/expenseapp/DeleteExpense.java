292a03a37d503988299541be733a48deb5b40813
atulbanwar <atulbanwar@gmail.com>
2016-09-10 19:38:14 -0400

    - Added Delete Expense Activity

--- app/src/main/java/com/homework/mad/expenseapp/DeleteExpense.java
+++ app/src/main/java/com/homework/mad/expenseapp/DeleteExpense.java
@@ -3,11 +3,11 @@ package com.homework.mad.expenseapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class EditActivity extends Activity {
+public class DeleteExpense extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_expense);
+        setContentView(R.layout.activity_delete_expense);
     }
 }

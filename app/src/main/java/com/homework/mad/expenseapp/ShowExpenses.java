47d27ac2a81390172dfb4f2bc1e5bbdb590e822c
sanketpat <sanket.patil30@gmail.com>
2016-09-10 19:16:55 -0400

    Added entries in string.xml

---
+++ app/src/main/java/com/homework/mad/expenseapp/ShowExpenses.java
@@ -0,0 +1,13 @@
+package com.homework.mad.expenseapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowExpenses extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_expenses);
+    }
+}

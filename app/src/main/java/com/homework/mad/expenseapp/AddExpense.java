43d9edcd51f27233df1bed60ff001fca98a9b10c
atulbanwar <atulbanwar@gmail.com>
2016-09-10 18:32:45 -0400

    - Added Add Expense Activity - Added Images in Drawable folder

---
+++ app/src/main/java/com/homework/mad/expenseapp/AddExpense.java
@@ -0,0 +1,13 @@
+package com.homework.mad.expenseapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddExpense extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}

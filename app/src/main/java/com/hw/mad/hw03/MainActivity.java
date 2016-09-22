f7b595f292d31d7f15d173295e4c4429b51e1d08
sanketpat <sanket.patil30@gmail.com>
2016-09-22 11:42:29 -0400

    -Added ui changes in main activity -Added Async task -Added png file

--- app/src/main/java/com/hw/mad/hw03/MainActivity.java
+++ app/src/main/java/com/hw/mad/hw03/MainActivity.java
@@ -2,12 +2,27 @@ package com.hw.mad.hw03;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Button btnExit, btnStartTrivia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnExit=(Button)findViewById(R.id.button_exit);
+        btnStartTrivia=(Button) findViewById(R.id.button_start_trivia);
+
+
+    }
+
+    public void startTrivia(View view) {
+    }
+
+    public void exit(View view) {
+        finish();
+        System.exit(0);
     }
 }

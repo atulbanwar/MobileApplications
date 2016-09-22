aaecacba7d2b786058b07134f5324e04b0bc3a53
sanketpat <sanket.patil30@gmail.com>
2016-09-22 17:29:47 -0400

    few changes

--- app/src/main/java/com/hw/mad/hw03/MainActivity.java
+++ app/src/main/java/com/hw/mad/hw03/MainActivity.java
@@ -1,7 +1,6 @@
 package com.hw.mad.hw03;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -19,8 +18,12 @@ public class MainActivity extends Activity implements GetTriviaData.IData {
 
     private Button btnExit, btnStartTrivia;
     private ProgressBar pgDiagLoading;
-    private ImageView imageViewTrivia;
-    private TextView textViewLoadingReady;
+    private ImageView imgViewTrivia;
+    private TextView txtViewLoadingReady;
+    private ArrayList<Question> questionList;
+    public static final String QUESTIONS_LIST_KEY = "QUESTIONS";
+    public static final int REQ_CODE_START = 1;
+    public static final String JSON_URL = "http://dev.theappsdr.com/apis/trivia_json/index.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +31,27 @@ public class MainActivity extends Activity implements GetTriviaData.IData {
         setContentView(R.layout.activity_main);
         btnExit = (Button) findViewById(R.id.button_exit);
         btnStartTrivia = (Button) findViewById(R.id.button_start_trivia);
-        imageViewTrivia = (ImageView) findViewById(R.id.image_view_trivia);
+        imgViewTrivia = (ImageView) findViewById(R.id.image_view_trivia);
         pgDiagLoading = (ProgressBar) findViewById(R.id.progress_bar_loading);
-        textViewLoadingReady = (TextView) findViewById(R.id.text_loading_ready);
-
+        txtViewLoadingReady = (TextView) findViewById(R.id.text_loading_ready);
         btnStartTrivia.setEnabled(false);
-        if (isConnectedOnline()) {
-            pgDiagLoading.setVisibility(View.VISIBLE);
-            textViewLoadingReady.setText(getResources().getString(R.string.progess_bar_loading));
-            new GetTriviaData(this).execute("http://dev.theappsdr.com/apis/trivia_json/index.php");
 
-        } else {
-            Toast.makeText(MainActivity.this, "Check your internet connection.", Toast.LENGTH_SHORT).show();
+        questionList = new ArrayList<Question>();
+
+        if (isConnectedOnline() && questionList.isEmpty()) {
+            pgDiagLoading.setVisibility(View.VISIBLE);
+            txtViewLoadingReady.setText(getResources().getString(R.string.progress_bar_loading));
+            new GetTriviaData(this).execute(JSON_URL);
+        } else if (isConnectedOnline()) {
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.error_no_internet), Toast.LENGTH_SHORT).show();
         }
 
     }
 
     public void startTrivia(View view) {
+        //Intent intent = new Intent(MainActivity.this, TriviaActivity.class);
+        //intent.putExtra(QUESTIONS_LIST_KEY, questionList);
+        //startActivityForResult(intent, REQ_CODE_START);
     }
 
     public void exit(View view) {
@@ -66,16 +73,16 @@ public class MainActivity extends Activity implements GetTriviaData.IData {
     public void setupData(ArrayList<Question> result) {
 
         pgDiagLoading.setVisibility(View.INVISIBLE);
-        textViewLoadingReady.setText("");
+        txtViewLoadingReady.setText("");
         if (!result.isEmpty()) {
 
-            textViewLoadingReady.setText(getResources().getString(R.string.progess_bar_ready));
-            imageViewTrivia.setImageResource(R.drawable.trivia);
+            txtViewLoadingReady.setText(getResources().getString(R.string.progress_bar_ready));
+            imgViewTrivia.setImageResource(R.drawable.trivia);
             btnStartTrivia.setEnabled(true);
+            questionList.addAll(result);
             //Your Question list is available here
-        }else
-        {
-            textViewLoadingReady.setText(getResources().getString(R.string.no_questions));
+        } else {
+            txtViewLoadingReady.setText(getResources().getString(R.string.error_no_questions));
         }
 
     }

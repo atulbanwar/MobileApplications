4160b755c6cf3ee1c567454d9d069c7db1d895e4
atulbanwar <atulbanwar@gmail.com>
2016-10-14 02:52:39 -0400

    - Removed copied comments

--- app/src/main/java/com/assgn/mad/weatherapp/SavedCitiesAdapter.java
+++ app/src/main/java/com/assgn/mad/weatherapp/SavedCitiesAdapter.java
@@ -22,39 +22,26 @@ import java.util.List;
 public class SavedCitiesAdapter extends
         RecyclerView.Adapter<SavedCitiesAdapter.ViewHolder> {
 
-    // ... view holder defined above...
-
-    // Store a member variable for the contacts
     private List<City> cities;
     // Store the context for easy access
     private Context mContext;
 
-    // Pass in the contact array into the constructor
     public SavedCitiesAdapter(Context context, List<City> cities) {
         this.cities = cities;
         mContext = context;
     }
 
-    // Easy access to the context object in the recyclerview
     private Context getContext() {
         return mContext;
     }
 
-    // Provide a direct reference to each of the views within a data item
-    // Used to cache the views within the item layout for fast access
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // Your holder should contain a member variable
-        // for any view that will be set as you render a row
         public TextView textViewSavedCityState;
         public TextView textViewSavedTemp;
         public TextView textViewSavedUpdatedDate;
         public ImageView imageViewFavouriteStar;
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
         public ViewHolder(View itemView) {
-            // Stores the itemView in a public final member variable that can be used
-            // to access the context from any ViewHolder instance.
             super(itemView);
 
             textViewSavedCityState = (TextView) itemView.findViewById(R.id.textViewSavedCityState);
@@ -69,20 +56,16 @@ public class SavedCitiesAdapter extends
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        // Inflate the custom layout
         View contactView = inflater.inflate(R.layout.saved_city_item_layout, parent, false);
 
-        // Return a new holder instance
         ViewHolder viewHolder = new ViewHolder(contactView);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(SavedCitiesAdapter.ViewHolder holder, int position) {
-        // Get the data model based on position
         City city = cities.get(position);
 
-        // Set item views based on your views and data model
         TextView textViewSavedCityState = holder.textViewSavedCityState;
         textViewSavedCityState.setText(city.getCity() + ", " + city.getCountry());
 

4191526b1b2b1d6bddae007bec4520614cdcd326
Sanket Patil <sanket.patil30@gmail.com>
2016-10-31 21:00:12 -0400

    Added SignUp activity

---
+++ app/src/main/java/com/example/mad/inclassassgn09/SignUp.java
@@ -0,0 +1,25 @@
+package com.example.mad.inclassassgn09;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class SignUp extends Activity {
+
+    private TextView firstName, lastName, email, password, confirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void actionCancel(){
+
+    }
+
+    public void actionSignUpSecond(){
+
+    }
+
+}

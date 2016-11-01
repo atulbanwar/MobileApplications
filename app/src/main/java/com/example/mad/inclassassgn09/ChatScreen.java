7499bc28cd2c5f8e10350b51d574d8ed752f6217
Sanket Patil <sanket.patil30@gmail.com>
2016-10-31 21:40:33 -0400

    Added chat scree

---
+++ app/src/main/java/com/example/mad/inclassassgn09/ChatScreen.java
@@ -0,0 +1,23 @@
+package com.example.mad.inclassassgn09;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+    }
+
+    public void actionSend()
+    {
+
+    }
+
+    public void actionAddPhoto()
+    {
+
+    }
+}

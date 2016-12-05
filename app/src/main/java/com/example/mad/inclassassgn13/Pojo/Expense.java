1f4f6a0e3d74d9f7e4ba5af018994509841c030d
atulbanwar <atulbanwar@gmail.com>
2016-12-05 18:51:58 -0500

    - Added Expense Pojo

---
+++ app/src/main/java/com/example/mad/inclassassgn13/Pojo/Expense.java
@@ -0,0 +1,48 @@
+package com.example.mad.inclassassgn13.Pojo;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by atulb on 10/17/2016.
+ */
+
+public class Expense extends RealmObject {
+    private String name;
+    private String category;
+    private double amount;
+    private Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
